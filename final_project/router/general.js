860e6acf35aa42f883275fcdbf5f575035014e01
Osagie Ehigiato <osg.csprog@gmail.com>
2025-01-24 20:03:28 -0500

    task 10 and 11 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -40,11 +41,39 @@ public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4));
 });
 
+public_users.get('/books', async function (req, res) {
+  try {
+    // Await the response from the Axios POST request
+    let response = await axios.get('https://osgcsprog-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/');
+    console.log(response.data); 
+    return res.send(JSON.stringify(response.data, null,4));
+    
+  } catch (error) {
+    // If there is an error, log the error message to the console
+    console.error('Error getting data:', error); 
+    return res.status(500).send("Internal error");
+  }
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
     res.send(JSON.stringify(books[isbn], null,4));
  });
+
+ public_users.get('/books/isbn/:isbn', async function (req, res) {
+    try {
+    const isbn = req.params.isbn;
+      let response = await axios.get('https://osgcsprog-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/'+isbn);
+      console.log(response.data); 
+      return res.send(JSON.stringify(response.data, null,4));
+      
+    } catch (error) {
+      // If there is an error, log the error message to the console
+      console.error('Error getting data:', error); 
+      return res.status(500).send("Internal error");
+    }
+  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
